74fcc89e4fd77bc15a63c36eb7aae6b62ab97808
Harini Kandauda <harini.kandauda@hotmail.fr>
2025-02-21 14:49:45 +0100

    added ChatProvider components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
-import Ecrire from "./Ecrire.jsx";
 import Contacts from "./Contacts.jsx";
 import { PersonProvider } from "./PersonContext.jsx";
 import Discussion from "./Discussion.jsx";
-// import Discussion from "./Discussion.jsx";
+import { ChatProvider } from "./ChatProvider.jsx";
 
 export default function App() {
   return (
     <PersonProvider>
-    <div style={{ display: "flex" }}>
-      <Contacts />
-      <Discussion/>
-    </div>
+      <div style={{ display: "flex" }}>
+        <Contacts />
+        <ChatProvider>
+          <Discussion />
+        </ChatProvider>
+      </div>
     </PersonProvider>
   );
 }
--- src/Discussion.jsx
+++ src/Discussion.jsx
@@ -2,9 +2,12 @@ import { useState, useContext } from "react";
 import { people } from "./data.jsx";
 import { PersonContext } from "./PersonContext.jsx";
 import Ecrire from "./Ecrire.jsx";
+import { useChat } from "./ChatProvider.jsx";
 
 function Discussion({ person }) {
   const { selectedPerson } = useContext(PersonContext);
+
+  const { messages, sendMessage, clearMessages, switchRoom } = useChat();
 
   return (
     <div className="Discussion">
@@ -14,6 +17,16 @@ function Discussion({ person }) {
           <li key={index}>{sms.text}</li>
         ))}
       </ul>
+      <div>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg.author}: {msg.content}</li>
+        ))}
+      </ul>
+      <button onClick={() => sendMessage('User', 'Hello!')}>Send Message</button>
+      <button onClick={clearMessages}>Clear Messages</button>
+      <button onClick={() => switchRoom('general')}>Switch to General Room</button>
+    </div>
       <Ecrire/>
     </div>
   );
